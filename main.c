61565276aacf5d910f1af46470e1917ea00b9978
KOEHLER Stéphane <stephane.koehler@fr.thalesgroup.com>
2022-05-26 20:30:01 +0200

    draw buffer

--- main.c
+++ main.c
@@ -1,21 +1,66 @@
 
 #include <SDL2/SDL.h>
- 
-int main(int argc, char *argv[])
+#include "stdint.h"
+
+static SDL_Renderer *gSDLRenderer;
+static SDL_Texture *texture;
+#define WIDTH 1024
+#define HEIGHT 768
+
+typedef uint32_t T_PixelType;
+
+T_PixelType gFrameBuffer[WIDTH*HEIGHT];
+
+static void createWindow()
 {
- 
     // returns zero on success else non-zero
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
 
+    SDL_Window* window = SDL_CreateWindow("GAME",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       WIDTH, HEIGHT, 0);
+
+
+    gSDLRenderer = SDL_CreateRenderer(window, -1, 0);
+#ifdef RENDER8888
+    texture = SDL_CreateTexture(gSDLRenderer,
+    SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+#else
+    texture = SDL_CreateTexture(gSDLRenderer,
+    SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+#endif
+
+}
+static void renderScene()
+{
+    SDL_RenderClear(gSDLRenderer);
+
+    for(int i=0;i<20000;i++)
+    {
+        gFrameBuffer[i]=0x00FF00FF;
+    }
+
+    SDL_UpdateTexture(texture , NULL, (const void *)gFrameBuffer, WIDTH * sizeof (T_PixelType));
+
+    SDL_RenderCopy(gSDLRenderer, texture, NULL, NULL);
+    SDL_RenderPresent(gSDLRenderer);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+
+    createWindow();
 
     SDL_Event e;
     for (;;) {
+
+        renderScene();
+
         SDL_PollEvent(&e);
         switch (e.type) {
             case SDL_QUIT:
