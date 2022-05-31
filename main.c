d613b318514042c2eae99c8658c37ed7703f1faf
Stéphane KOEHLER <stephane.koehler@fr.thalesgroup.com>
2022-05-31 20:21:34 +0200

    terrain tile test

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include "stdint.h"
 #include "stdio.h"
+#include "map.h"
 extern void perlinGenGradiant();
 extern void perlinGenTexture(uint32_t * pBuffer,int pWidth,int pHeight);
 static SDL_Renderer *gSDLRenderer;
@@ -12,6 +13,7 @@ static SDL_Texture *texture;
 typedef uint32_t T_PixelType;
 
 T_PixelType gFrameBuffer[WIDTH*HEIGHT];
+
 
 static void createWindow()
 {
@@ -45,12 +47,28 @@ static void renderScene()
         gFrameBuffer[i]=0x00FF00FF;
     }
 
-    
     perlinGenTexture(gFrameBuffer,WIDTH,HEIGHT);
 
-    SDL_UpdateTexture(texture , NULL, (const void *)gFrameBuffer, WIDTH * sizeof (T_PixelType));
+    T_map_ctrl *lMapCtrl = map_getMapControl();
+    T_map_texture * lTextures = map_getMapTexture();
 
+    SDL_UpdateTexture(texture , NULL, (const void *)gFrameBuffer, WIDTH * sizeof (T_PixelType));
+    for(int i=0;i<9;i++)
+    {
+        SDL_Rect lRect;
+        lRect.x = lTextures[i].posX*K_MAP_TILE_DIMENSION - (int)lMapCtrl->posx + WIDTH/2;
+        lRect.y = lTextures[i].posY*K_MAP_TILE_DIMENSION- (int)lMapCtrl->posy + HEIGHT/2;
+        lRect.h=K_MAP_TILE_DIMENSION;       
+        lRect.w=K_MAP_TILE_DIMENSION;       
+        SDL_UpdateTexture(texture , &lRect, (const void *)lTextures[i].buffer, K_MAP_TILE_DIMENSION * sizeof (T_PixelType));
+    }
     SDL_RenderCopy(gSDLRenderer, texture, NULL, NULL);
+
+
+    SDL_SetRenderDrawColor(gSDLRenderer, 255, 0, 0, 255);
+    SDL_RenderDrawLine(gSDLRenderer, WIDTH/2 - 10, HEIGHT/2, WIDTH/2 + 10, HEIGHT/2);
+    SDL_SetRenderDrawColor(gSDLRenderer, 255, 0, 0, 255);
+    SDL_RenderDrawLine(gSDLRenderer, WIDTH/2, HEIGHT/2-10, WIDTH/2, HEIGHT/2 + 10);
     SDL_RenderPresent(gSDLRenderer);
 }
 
@@ -61,6 +79,7 @@ int main(int argc, char *argv[])
 
     createWindow();
     perlinGenGradiant();
+    T_map_ctrl *lMapCtrl = map_getMapControl();
 
     SDL_Event e;
     for (;;) {
@@ -74,13 +93,29 @@ int main(int argc, char *argv[])
                     SDL_Log("Program quit after %i ticks", e.quit.timestamp);
                     exit(0);
                     break;
-                case SDL_KEYDOWN:              
-                    perlinGenGradiant();
-                    break;
+                case SDL_KEYDOWN:    
+                    switch(e.key.keysym.sym){
+                    case SDLK_UP : 
+                        lMapCtrl->posy -=10;              
+                        break;
+                    case SDLK_DOWN :    
+                        lMapCtrl->posy +=10;       
+                        break;
+                    case SDLK_LEFT :   
+                        lMapCtrl->posx -=10;             
+                        break;
+                    case SDLK_RIGHT :    
+                        lMapCtrl->posx +=10;      
+                        break;
+                    default:   
+                        perlinGenGradiant();          
+                        break;
+                    }
                 default:
                     break;
             }
         }
         SDL_Delay(10);
+        map_refresh();
     }
 }
